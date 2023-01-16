818eb5dc867ea504ca8a53f70b18bf99f06d9aa1
IGardocki <ian.d.gardocki@gmail.com>
2023-01-15 21:07:34 -0600

    updated activity info

--- src/components/ActivityInfo.tsx
+++ src/components/ActivityInfo.tsx
@@ -1,17 +1,44 @@
 import React from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { activityState } from "./recoil/atoms";
+import { Card } from "react-bootstrap";
 
 
-export const ActivityInfo: React.FC = () =>{
+export const ActivityInfo: React.FC = () => {
     const activity = useRecoilValue(activityState);
     console.log(activity)
-    const test = 3
-    return(
-        <div data-testid='activity-info'>
-            <h1>{activity.activity}</h1>
-            {/* <h1>{test}</h1> */}
-        </div>
-    
+    return (
+        <Card data-testid='activity-info'>
+            <Card.Title>{activity.activity}</Card.Title>
+            <Card.Body>
+                <Card.Text>
+                    accessibility: {activity.accessibility}
+                </Card.Text>
+                <Card.Text>
+                    key: {activity.key}
+                </Card.Text>
+                <Card.Text>
+                    link: {activity.link}
+                </Card.Text>
+                <Card.Text>
+                    participants: {activity.participants}
+                </Card.Text>
+                <Card.Text>
+                    price: {activity.price}
+                </Card.Text>
+                <Card.Text>
+                    type: {activity.type}
+                </Card.Text>
+            </Card.Body>
+
+            {/* accessibility: 0,
+        activity: '',
+        key: "0",
+        link: "",
+        participants: 1,
+        price: 0.1,
+        type: "" */}
+        </Card>
+
     )
 }
